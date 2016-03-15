9789226f9f565ca8a52547f296e45f69287c2165
Dega <a.degaspari@18months.it>
2016-03-15 19:31:18 +0100

    firma ok, verifica ko

--- lib/http_signatures/algorithm.rb
+++ lib/http_signatures/algorithm.rb
@@ -2,10 +2,11 @@ module HttpSignatures
   module Algorithm
 
     def self.create(name)
-      case name
+      case name.downcase
       when "hmac-sha1" then Hmac.new("sha1")
       when "hmac-sha256" then Hmac.new("sha256")
       when "rsa-sha256" then Rsa.new("sha256")
+      when "rsa_sha256" then Rsa.new("sha256")
       else raise UnknownAlgorithm.new(name)
       end
     end
